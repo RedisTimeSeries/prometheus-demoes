baa10b7e65900bc9db7deed644a8d5610f4b50a4
Pieter Cailliau <pieter.cailliau@neotechnology.com>
2019-06-07 11:38:48 +0100

    using pipeline

--- weather_station/sensors.py
+++ weather_station/sensors.py
@@ -12,13 +12,14 @@ def main(host, port, sensor_count):
     r = redis.Redis(host=host, port=port)
 
     while True:
+        pipeline = r.pipeline(transaction=False)
         for sensor_id in range(0, sensor_count):
-            r.execute_command('TS.ADD', 'temperature:%d' % (sensor_id,), '*', random.randrange(10, 20),
+            pipeline.execute_command('TS.ADD', 'temperature:{%d}' % (sensor_id,), '*', random.randrange(10, 20),
                               'LABELS', '__name__', 'temperature', 'sensor', sensor_id)
-            r.execute_command('TS.ADD', 'humidity:%d' % (sensor_id,), '*', random.randrange(70, 80),
+            pipeline.execute_command('TS.ADD', 'humidity:{%d}' % (sensor_id,), '*', random.randrange(70, 80),
                               'LABELS', '__name__', 'humidity', 'sensor', sensor_id)
+        pipeline.execute()
         time.sleep(1)
-
 
 if __name__ == '__main__':
     main()
